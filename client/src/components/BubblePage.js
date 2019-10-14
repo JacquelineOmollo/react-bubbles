d3c6a1ce3c9bfbd722c558dfe3c6585826bc1e6f
Jacqueline Omollo <jacquelineOmolloo@gmail.com>
2019-10-13 21:24:36 -0400

    First commit with setup in some components built

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -8,6 +8,19 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+  useEffect(() => {
+    theColors();
+  }, []);
+
+  const theColors = () => {
+    axiosWithAuth()
+      .get("http://localhost:5000/api/colors")
+      .then(res => {
+        console.log("How about those colors", res);
+        setColorList(res.data);
+      })
+      .catch(err => console.log(err.res));
+  };
 
   return (
     <>
--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -21,10 +21,17 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put("http://localhost:5000/api/colors/${colorToEdit.id", colorToEdit)
+      .then(res => {
+        localStorage.setItem("token", res.data.token);
+        this.props.history.push("/");
+      });
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth().delete(`http://localhost:5000/api/colors/${color.id}`);
   };
 
   return (
